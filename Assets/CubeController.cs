6dbae27696595dc91f30a009e22bcf1a981dfcc2
yamazaki0305 <yamazaki0305@hotmail.com>
2018-09-11 02:47:15 +0900

    Lesson9までコミット

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+        // ゲームオーバテキスト
+        private GameObject gameOverText;
+        
+        // 走行距離テキスト
+        private GameObject runLengthText;
+
+        // 走った距離
+        private float len = 0;
+
+        // 走る速度
+        private float speed = 0.03f;
+
+        // ゲームオーバの判定
+        private bool isGameOver = false;
+
+        // Use this for initialization
+        void Start () {
+                // シーンビューからオブジェクトの実体を検索する
+                this.gameOverText  = GameObject.Find ("GameOver");
+                this.gameOverText.SetActive(false);
+                this.runLengthText = GameObject.Find ("RunLength");
+        }
+
+        // Update is called once per frame
+        void Update () {
+            if (this.isGameOver == false)
+            {
+                // 走った距離を更新する
+                this.len += this.speed;
+                //Debug.Log(this.len);
+                // 走った距離を表示する
+                this.runLengthText.GetComponent<Text>().text = "Distance:  " + len.ToString("F2") + "m";
+            }
+            // ゲームオーバになった場合
+            if (this.isGameOver )
+            {
+                // クリックされたらシーンをロードする（追加）
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //GameSceneを読み込む（追加）
+                    SceneManager.LoadScene("GameScene");
+                }
+            }
+            
+        }
+    
+        
+        public void GameOver()
+        {
+        // ゲームオーバになったときに、画面上にゲームオーバを表示する
+        Debug.Log("gameover");
+                this.gameOverText.SetActive(true);
+                this.gameOverText.GetComponent<Text>().text = "GameOver";
+                this.isGameOver = true;
+        }
+}
